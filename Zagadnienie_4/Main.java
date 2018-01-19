1e3f625f10066b6c94b3605d18c7da7fffb559aa
Pucior <32542539+Pucior@users.noreply.github.com>
2018-01-19 11:18:12 +0100

    Add files via upload

--- Zagadnienie_4/Main.java
+++ Zagadnienie_4/Main.java
@@ -1,20 +1,20 @@
 public class Main {
     public static void main(String[]args) {
-        double learningrate = 0.01;
+        double learningrate = 0.1;
+        double forgetrate = 0.1;
         double damagedAOutput = 1;
         int lettersToLearning = 20;
         Dataset ds = new Dataset();
         ds.generateWeights(ds.weights);
-        for(int k = 0; k < 100; k++) {
+        for(int k = 0; k < 20; k++) {
             for (int i = 0; i < lettersToLearning; i++) {
                 for (int j = 0; j < 35; j++) {
-                    ds.weights[j] = ds.weights[j] + learningrate * ds.letters[i][j] * ds.outputs[i];
+                    ds.weights[j] = (1 - forgetrate) * ds.weights[j] + learningrate * ds.letters[i][j] * ds.outputs[i];
                 }
                 for (int j = 0; j < 35; j++){
                     ds.outputs[i] = ds.weights[j] * ds.letters[i][j];
                 }
             }
-            if (k % 5 == 0)
                 lettersToLearning --;
         }
         for (int j = 0; j < 35; j++) {
